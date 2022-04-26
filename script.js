f0053eac17a19a616a9b4696ec8c59e0476525d5
Marlon Almino <alminomarlon@gmail.com>
2022-04-26 14:08:35 -0300

    start script, general function to movimentation

---
+++ script.js
@@ -0,0 +1,28 @@
+const yourShip = document.querySelector('.player-shooter')
+const playArea = document.querySelector('#main-play-game')
+
+// Movimento e tiro da nave
+function flyShip(event) {
+  if (event.key === 'ArrowUp') {
+    event.preventDefault()
+    moveUp()
+  } else if (event.key === 'ArrowDown') {
+    event.preventDefault()
+    moveDown()
+  } else if (event.key === ' ') {
+    event.preventDefault()
+    fireLaser()
+  }
+}
+
+// Função de Subir
+function moveUp() {
+  let topPosition = getComputedStyle(yourShip).getPropertyValue('top')
+  if (topPosition === '0px') {
+    return
+  } else {
+    let position = parseInt(topPosition)
+    position -= 50
+    yourShip.style.top = `${position}px`
+  }
+}
